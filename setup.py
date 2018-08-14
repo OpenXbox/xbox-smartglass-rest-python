367d8c5eb2d9f0694d01107cdac2e5c8d8e81274
tuxuser <462620+tuxuser@users.noreply.github.com>
2018-08-14 15:24:35 +0200

    Bump version: 0.9.3 → 0.9.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="xbox-smartglass-rest",
-    version="0.9.3",
+    version="0.9.4",
     author="OpenXbox",
     description="Xbox One Smartglass REST API",
     long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
