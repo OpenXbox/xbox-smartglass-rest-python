facb77f67d90137f4209ed7cf707e7a8c05dd6fa
tuxuser <462620+tuxuser@users.noreply.github.com>
2018-11-05 12:37:31 +0100

    Filter returned consoles when addr-query is supplied

--- xbox/rest/routes/device.py
+++ xbox/rest/routes/device.py
@@ -29,7 +29,9 @@ def device_overview():
     for d in discovered:
         app.console_cache.update({d.liveid: ConsoleWrap(d)})
 
-    data = {console.liveid: console.status for console in app.console_cache.values()}
+    # Filter for specific console when ip address query is supplied (if available)
+    data = {console.liveid: console.status for console in app.console_cache.values()
+                if (addr and console.status.get('address') == addr) or not addr}
     return app.success(devices=data)
 
 
